b0a02d8e9939637865fc1a93a2d93718ec1c7cfd
Guilherme <guilherme.simbalista@yahoo.com.br>
2023-10-11 17:39:57 -0300

    Created Project

---
+++ src/test/java/com/guilhermesimbalista/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesimbalista.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
